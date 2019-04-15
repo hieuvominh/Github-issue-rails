1b9757ed1eb8046d3c8ef0ca673d43593b8da073
hieuvominh56@gmail.com <hieuvominh56@gmail.com>
2019-04-14 21:37:33 -0700

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,11 +8,11 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  config.eager_load = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
